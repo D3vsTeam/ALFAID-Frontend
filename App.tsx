85aef05596dea7a319ab96835ce38730198d205a
IagoAntunes <iagoantunes.f@gmail.com>
2022-08-06 01:44:15 -0300

    Home, Form

--- App.tsx
+++ App.tsx
@@ -1,12 +1,10 @@
-import { View,Text } from 'react-native';
 import React from 'react';
-<<<<<<< HEAD
+
 import { Form, Home } from './src/screens';
-=======
+
 import { StyleSheet, Text, View } from 'react-native';
-import { Home } from './src/screens';
 import Auth from './src/screens/Auth';
->>>>>>> 5eccce5faf96faf074b70b7315bf0b8765e0a4de
+
 
 
 export default function App() {
