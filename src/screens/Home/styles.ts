b211407b5b69365ecce460a055e36315e1ef800c
IagoAntunes <iagoantunes.f@gmail.com>
2022-08-06 03:31:10 -0300

    SignOut,  Styles

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -12,14 +12,40 @@ export const HeaderAll = styled.View`
     flex:1;
 `
 
-export const viewButtons = styled.View`
-    background-color: darkblue
+export const ViewButtons = styled.View`
+    flex: 1
+    padding: 20px
+    align-Items: flex-start
+    justify-Content: flex-start
+    margin: 20px
 `
 
-export const btn = styled.TouchableOpacity`
-background-color: darkblue 
+export const CustomBotoes = styled.TouchableOpacity`
+    height: 110px
+    width: 300px
+    borderWidth: 0.2px
+    borderRadius: 30px
+    flexDirection: row
 `
 
 export const SignOut = styled.TouchableOpacity`
 
+`
+
+export const TextNome = styled.Text`
+    color: white
+    margin-Right: 20px
+    font-Size:20px
+`
+
+export const TextTitulo = styled.Text`
+    color: white
+    margin-Right: 20px
+    font-Size:30px
+    margin-Top: 30px
+`
+
+export const TextBtn = styled.Text`
+    font-Size: 18px
+    font-Weight: bold
 `
