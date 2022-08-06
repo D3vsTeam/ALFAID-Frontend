1b41b4b0879ebfa5f915216319afd6c1bf823fbe
Ítalo Médici <59889993+ItaloMedici@users.noreply.github.com>
2022-08-06 16:43:02 -0300

    auth

--- src/screens/Auth/index.tsx
+++ src/screens/Auth/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Container, LogoImage, Title } from './styles';
 import logo from '../../assets/logo.png'
-import { Button, Flex, FormControl, Icon, Input, KeyboardAvoidingView, NativeBaseProvider, ScrollView, Text } from "native-base";
+import { Button, Flex, FormControl, Heading, HStack, Icon, Image, Input, InputGroup, KeyboardAvoidingView, NativeBaseProvider, ScrollView, Text, View, VStack } from "native-base";
 import { useAuth } from "../../hook/useAuth";
 import { Funcionario } from "../../models/Funcionario";
 import { AntDesign } from '@expo/vector-icons';
@@ -18,48 +18,69 @@ const Auth: React.FC = () => {
   }
 
   return (
-    <Container>
-    <KeyboardAvoidingView 
-    behavior ={"padding"}>
-      <ScrollView style={{width: "100%"}}>
-      <Title>
-        Login
-      </Title>
-      <LogoImage source={logo} />
-        <Flex 
-        alignItems={"center"}
-        height={120}
-        >
-          <FormControl.Label >Login</FormControl.Label>
-            <Input
-              h={50}
-              w="80%"
-              size="md"
-              alignItems={"center"}
-              justifyContent ={"center"}
-              variant="rounded"
-              InputLeftElement={<Icon as={<AntDesign Cpf="user" />} size={5} ml="2" color="muted.400" />} placeholder="Nome..." 
-              onChangeText={(text: string) => setValues({...values, cpf: text})}
-            />
-        </Flex>
-        <Flex alignItems={"center"} height={"140"}>
-          <FormControl.Label>Password</FormControl.Label>
-          <Input
-            size="md"
-            w="80%"
-            variant="rounded"
-            type={show ? "text" : "password"} InputRightElement={<Icon as={<MaterialIcons name={values ? "visibility" : "visibility-off"} />} size={5} mr="2" color="muted.400" onPress={() => setShow(!show)} />} placeholder="Senha..."
-          onChangeText={(text: string) => setValues({...values, senha: text})}
-            />
-        </Flex>
-        <Flex alignItems={"center"}>
-          <Button borderRadius={30} alignItems={"center"} width= "80%" h={50} colorScheme= "primary" onPress={handleLogin}>
-            Login
-          </Button>
-        </Flex>
-    </ScrollView>
-    </KeyboardAvoidingView>
-    </Container>
+    <View flex={1} bgColor="white" h="full">
+      <KeyboardAvoidingView
+        behavior={"padding"}
+        h="full"
+        display={'flex'}
+      >
+        <ScrollView flex={1} display="flex" h="full">
+          <HStack flex={.5}>
+            <Heading>
+              Login
+            </Heading>
+            <Image source={logo} flex={1} />
+          </HStack>
+          <VStack
+            p={8}
+            space={4}
+            flex={1}
+            bgColor={"red.100"}
+            marginTop="auto"
+          >
+            <InputGroup flexDirection={"column"}>
+              <FormControl.Label>Login</FormControl.Label>
+              <Input
+                borderRadius={6}
+                size="md"
+                InputLeftElement={
+                  <Icon
+                    as={<AntDesign Cpf="user" />}
+                    size={5}
+                    color="muted.400"
+                  />
+                }
+                placeholder="Digite seu CPF..."
+                onChangeText={(text: string) => setValues({ ...values, cpf: text })}
+              />
+            </InputGroup>
+            <InputGroup flexDirection={"column"}>
+              <FormControl.Label>Password</FormControl.Label>
+              <Input
+                borderRadius={6}
+                size="md"
+                type={show ? "text" : "password"}
+                InputRightElement={
+                  <Icon
+                    as={<MaterialIcons name={values ? "visibility" : "visibility-off"} />}
+                    size={5} mr="2"
+                    color="muted.400"
+                    onPress={() => setShow(!show)}
+                  />}
+                placeholder="Digite a senha..."
+                onChangeText={(text: string) => setValues({ ...values, senha: text })}
+              />
+            </InputGroup>
+            <Button
+              onPress={handleLogin}
+              borderRadius={6}
+            >
+              Entrar
+            </Button>
+          </VStack>
+        </ScrollView>
+      </KeyboardAvoidingView>
+    </View>
   );
 };
 
