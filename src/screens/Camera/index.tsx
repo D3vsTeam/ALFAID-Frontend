c65ec84500163f68528ee1b52b1eeffc3a0a5a70
arleynm <arleyaugusto5@gmail.com>
2022-08-07 11:55:18 -0300

    camera

--- src/screens/Camera/index.tsx
+++ src/screens/Camera/index.tsx
@@ -1,122 +1,95 @@
 import React, { useState } from "react";
-import { View, Text, TouchableOpacity, StyleSheet, Modal, Image, SafeAreaView } from "react-native";
-import { PickImage } from '../../components'
-import { Camera, CameraType } from "expo-camera";
+import { View, Text, StyleSheet, Image, SafeAreaView, StatusBar, Button } from "react-native";
+import { Camera, CameraCapturedPicture } from "expo-camera";
 import { useEffect, useRef } from "react";
+import { shareAsync } from "expo-sharing"
 import { FontAwesome } from "@expo/vector-icons"
-import * as Permissions from 'expo-permissions';
-import { MaterialIcons } from '@expo/vector-icons';
-
 import *as MediaLibrary from 'expo-media-library'
+import { background } from "native-base/lib/typescript/theme/styled-system";
 
 
 export const PageCamera = () => {
-  const camRef = useRef(null);
-  const [type, setType] = useState(Camera.Constants.Type.back);
-  const [hasPermission, setHasPermission] = useState(false);
-  const [capturedPhoto, setCapturePhoto] = useState(null);
-  const [open, setOpen] = useState(false);
+  const cameraRef = useRef<Camera>(null);
+  const [hasCameraPermission, setHasCameraPermission] = useState(false);
+  const [hasMediaLibraryPermission, sethasMediaLibraryPermission] = useState(false);
+  const [photo, setphoto] = useState<CameraCapturedPicture>();
+
 
   useEffect(() => {
     (async () => {
-      const { status } = await Permissions.askAsync();
-      setHasPermission(status === 'granted');
-      console.log(status);
+      const cameraPermission = await Camera.requestCameraPermissionsAsync();
+      const mediaLibraryPermission = await MediaLibrary.requestPermissionsAsync();
+      
+      setHasCameraPermission(cameraPermission.status === "granted");
+      sethasMediaLibraryPermission(mediaLibraryPermission.status === "granted");
     })();
   }, []);
 
-  if (hasPermission === null) {
-    return <View />;
-  }
-  if (hasPermission === false) {
-    return <Text> Acesso negado!</Text>;
+  if (hasCameraPermission === undefined) {
+    return <Text>Requerindo permissões ...</Text>
+  } else if (!hasCameraPermission) {
+    return <Text>Sem permissão!</Text>
   }
 
-  async function takePickute() {
-    if (camRef) {
-      const data = await camRef.current.takePicturesAsync();
-      setCapturePhoto(data.uri);
-      setOpen(true);
+  const takePic = async () => {
+    let options = {
+      quality: 1,
+      base64: true,
+      exif: false,
+
+    };
+    if (cameraRef.current) {
+      let newPhoto = await cameraRef.current.takePictureAsync(options);
+      setphoto(newPhoto);
     }
+
   }
 
-  async function savePicture() {
-    const asset = await MediaLibrary.createAssetAsync(capturedPhoto)
-      .then(() => {
-        alert('Salvo com sucesso')
+  if (photo) {
+    let sharePic = () => {
+      shareAsync(photo.uri).then(() => {
+        setphoto(undefined);
       })
+    };
+    let savePhoto = () => {
+      MediaLibrary.saveToLibraryAsync(photo.uri).then(() => {
+        setphoto(undefined);
+      })
+    };
+
+    return (
+      <SafeAreaView style={styles.container}>
+        <Image style={styles.preview} source={{ uri: "data:image/jpg:base64" + photo.base64 }} />
+        <Button title="Share" onPress={sharePic} />
+        {hasMediaLibraryPermission ? <Button title="Save" onPress={savePhoto} /> : undefined}
+        <Button title="Discard" onPress={() => setphoto(undefined)} />
+      </SafeAreaView>
+    );
+
   }
 
   return (
-    <SafeAreaView style={styles.container}>
-      <Camera style={{ flex: 1 }}
-        type={type}
-        ref={camRef}
-      >
-        <View style={{ flex: 1, backgroundColor: 'transparent', flexDirection: 'row' }}>
-          <TouchableOpacity
-            style={{
-              position: 'absolute',
-              bottom: 20,
-              left: 20,
-            }}
-            onPress={() => {
-              setType(type === Camera.Constants.Type.back
-                ? Camera.Constants.Type.front
-                : Camera.Constants.Type.back
-              );
-            }}>
-            <Text style={{ fontSize: 20, marginBottom: 13, color: "#fff" }}> Trocar</Text>
-          </TouchableOpacity>
-        </View>
-      </Camera>
-
-      <TouchableOpacity style={styles.button} onPress={takePickute}>
-        <FontAwesome name="camera" size={23} color='#FFF' />
-      </TouchableOpacity>
-      {
-        capturedPhoto &&
-        <Modal
-          animationType='slide'
-          transparent={false}
-          visible={open}
-        >
-          <View style={{ flex: 1, justifyContent: 'center', alignItems: ' center', margin: 20 }}>
-
-            <View style={{ margin: 10, flexDirection: 'row' }}>
-              <TouchableOpacity style={{ margin: 10 }} onPress={() => setOpen(false)}>
-                <FontAwesome name="window-close" size={50} color="#FF0000" />
-              </TouchableOpacity>
-
-              <TouchableOpacity style={{ margin: 10 }} onPress={() => setOpen(false)}>
-                <FontAwesome name="upload" size={50} color="#121212" />
-              </TouchableOpacity>
-            </View>
-            <Image
-              style={{ width: '100%', height: 450, borderRadius: 20 }}
-              source={{ uri: capturedPhoto }}
-            />
-
-          </View>
-        </Modal>
-      }
-    </SafeAreaView>
-  )
+    <Camera style={styles.container} ref={cameraRef}>
+      <View style={styles.buttonContainer}>
+        <Button title="take pic" onPress={takePic} />
+      </View>
+    </Camera>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center'
   },
-  button: {
-    justifyContent: 'center',
-    alighItems: 'center',
-    backgroundColor: '#121212',
-    margin: 20,
-    borderRadius: 10,
-    height: 50,
+  buttonContainer: {
+    backgroundColor: 'center',
+    alignItems: 'flex-end',
+  },
+  preview: {
+    alignSelf: 'stretch',
+    flex: 1,
   }
+
 });
