32437c83b2dbd610893e3e4fb975774b76ac0580
Ítalo Médici <59889993+ItaloMedici@users.noreply.github.com>
2022-08-07 04:45:18 -0300

    Import

--- src/screens/PageImg/index.tsx
+++ src/screens/PageImg/index.tsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import * as ImagePicker from 'expo-image-picker';
 import { Ionicons } from '@expo/vector-icons';
 import { useNavigation } from "@react-navigation/native";
+import { useTarefa } from "../../hook/useTarefa";
 
 export const PageImg = () => {
   const { tarefa } = useTarefa();
