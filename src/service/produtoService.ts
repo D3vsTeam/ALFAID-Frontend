6d6681095a71fdf9aad08800eb46012c859ec203
Ítalo Médici <59889993+ItaloMedici@users.noreply.github.com>
2022-08-07 01:25:40 -0300

    Adicionando rota de derivação

--- src/service/produtoService.ts
+++ src/service/produtoService.ts
@@ -1,7 +1,11 @@
 import { AxiosResponse } from "axios"
-import { Produto } from "../models/Produto"
+import { Derivacoes, Produto } from "../models/Produto"
 import { api } from "./api"
 
 export const getProdutos = () => {
   return api.get("/get-produtos") as Promise<AxiosResponse<Produto[], any>>
+}
+
+export const getDerivacoes = () => {
+  return api.get("/get-derivacoes") as Promise<AxiosResponse<Derivacoes[], any>>
 }
--- src/service/storageService.ts
+++ src/service/storageService.ts
@@ -1,12 +1,13 @@
 import AsyncStorage from "@react-native-async-storage/async-storage"
 import { getEquipe } from "./funcionarioService"
-import { getProdutos } from "./produtoService";
+import { getDerivacoes, getProdutos } from "./produtoService";
 
 export const buildOfflineData = async () => {
-  const [ equipes, produtos ] = await Promise.all([
-    getEquipe(), getProdutos()
+  const [ equipes, produtos, derivacoes ] = await Promise.all([
+    getEquipe(), getProdutos(), getDerivacoes()
   ]);
 
   await AsyncStorage.setItem("@AlfaID:equipes", JSON.stringify(equipes.data))
   await AsyncStorage.setItem("@AlfaID:produtos", JSON.stringify(produtos.data))
+  await AsyncStorage.setItem("@AlfaID:derivacoes", JSON.stringify(derivacoes.data))
 }
