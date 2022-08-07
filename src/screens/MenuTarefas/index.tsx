9d2c51f7fe28d83008b87c5395d253e932d55bf8
Ítalo Médici <59889993+ItaloMedici@users.noreply.github.com>
2022-08-06 23:56:01 -0300

    Menu

--- src/screens/MenuTarefas/index.tsx
+++ src/screens/MenuTarefas/index.tsx
@@ -1,68 +1,138 @@
 import React, { useEffect, useState } from "react";
-import { View, TextInput, Text ,StyleSheet, TouchableOpacity} from "react-native";
+import { View, TextInput, StyleSheet, TouchableOpacity } from "react-native";
 import { Feather } from '@expo/vector-icons';
 import { Ionicons } from '@expo/vector-icons';
 import { useNavigation, useRoute } from "@react-navigation/native";
 import { Tarefa } from "../../models/Tarefa";
 import { useRbc } from "../../hook/useRbc";
+import { Box, FormControl, HStack, Input, InputGroup, TextArea, VStack, Pressable, Text, Heading, Icon } from "native-base";
 
 
-export const MenuTarefas = ({  }) => {
-    const { rbc } = useRbc();
-    const navigation = useNavigation();
-    const route = useRoute();
-    const { tarefa } = route.params as { tarefa: Tarefa }
+export const MenuTarefas = ({ }) => {
+  const { rbc } = useRbc();
+  const navigation = useNavigation();
+  const route = useRoute();
+  const { tarefa } = route.params as { tarefa: Tarefa }
 
-    return(
-        <View style={styled.ViewMenu}>
-            <Text style={styled.textTitle}>Observações</Text>
-            <View style={{backgroundColor: 'lightgray',borderRadius: 5,padding: 10}}>
-            <TextInput textAlignVertical='top' multiline={true} numberOfLines={5} placeholder='Observação...' />
-            </View>
+  const mainProps = {
+    w: 150,
+    h: 150,
+    background: "gray.50",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "gray.300",
+    shadow: "1",
+  }
 
-            <View style={{flexDirection: 'row'}}>
-                <TouchableOpacity style={styled.buttons} onPress={()=> navigation.navigate('PageImg')}>
-                    <Feather name="image" size={24} color="black" />
-                    <Text>Imagem</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styled.buttons} onPress={()=> navigation.navigate('PageFunc')}>
-                    <Ionicons name="people" size={24} color="black" />
-                    <Text>Funcionarios</Text>
-                </TouchableOpacity>
-            </View>
-            <View style={{flexDirection: 'row'}}>
-                <TouchableOpacity style={styled.buttons} onPress={()=> navigation.navigate('PageAvaliacao')}>
-                    <Feather name="star" size={24} color="black" />
-                    <Text>Imagem</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styled.buttons} onPress={()=> navigation.navigate('PageFunc')}>
-                    <Ionicons name="people" size={24} color="black" />
-                    <Text>Funcionarios</Text>
-                </TouchableOpacity>
-            </View>
+  return (
+    <Box p={3}>
+      <FormControl.Label fontSize={"xl"}>Tarefa Realizada</FormControl.Label>
+      <TextArea
+        p={4}
+        autoCompleteType=""
+        fontSize={"xl"}
+        borderRadius={8}
+        size="4xl"
+        w={"full"}
+        placeholder="Digte aqui a tarefa realizada..."
+      />
+      <Heading my={4}>Opções</Heading>
+      <VStack w={"full"} space={8} >
+        <HStack space={8} justifyContent="center">
+          <Pressable
+            {...mainProps}
+            onPress={() => navigation.navigate('PageImg')}
+          >
+            <Ionicons name="image" size={30} color="rgba(0,0,0,.5)" />
+            <Text bold mt={3} fontSize={"xl"}>Imagem</Text>
+          </Pressable>
+          <Pressable
+            {...mainProps}
+            onPress={() => navigation.navigate('PageFunc')}
+          >
+            <Ionicons name="md-people" size={30} color="rgba(0,0,0,.5)" />
+            <Text bold mt={3} fontSize={"xl"}>Equipe</Text>
+          </Pressable>
+        </HStack>
+        <HStack space={8} justifyContent="center">
+          <Pressable
+            {...mainProps}
+            onPress={() => navigation.navigate('PageAvaliacao')}
+          >
+            <Ionicons name="star" size={30} color="rgba(0,0,0,.5)" />
+            <Text bold mt={3} fontSize={"xl"}>Avaliação</Text>
+          </Pressable>
+          <Pressable
+            {...mainProps}
+          //onPress={() => navigation.navigate('PageFunc')}
+          >
+            <Ionicons name="alert-circle" size={30} color="rgba(0,0,0,.5)" />
+            <Text bold mt={3} fontSize={"xl"}>Condições</Text>
+          </Pressable>
+        </HStack>
+        <HStack space={8} justifyContent="center">
+          <Pressable
+            {...mainProps}
+          //onPress={() => navigation.navigate('PageFunc')}
+          >
+            <Ionicons name="cube" size={30} color="rgba(0,0,0,.5)" />
+            <Text bold mt={3} fontSize={"xl"}>Produto</Text>
+          </Pressable>
+          <Pressable
+            {...mainProps}
+          //onPress={() => navigation.navigate('PageFunc')}
+          >
+            <Ionicons name="navigate" size={30} color="rgba(0,0,0,.5)" />
+            <Text bold mt={3} fontSize={"xl"}>Localização</Text>
+          </Pressable>
+        </HStack>
+      </VStack>
 
-        </View>
-    )
+      {/*       <View style={{ flexDirection: 'row' }}>
+        <TouchableOpacity style={styled.buttons} onPress={() => navigation.navigate('PageImg')}>
+          <Feather name="image" size={24} color="rgba(0,0,0,.5)" />
+          <Text>Imagem</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styled.buttons} onPress={() => navigation.navigate('PageFunc')}>
+          <Ionicons name="people" size={24} color="rgba(0,0,0,.5)" />
+          <Text>Funcionarios</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={{ flexDirection: 'row' }}>
+        <TouchableOpacity style={styled.buttons} onPress={() => navigation.navigate('PageAvaliacao')}>
+          <Feather name="star" size={24} color="rgba(0,0,0,.5)" />
+          <Text>Imagem</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styled.buttons} onPress={() => navigation.navigate('PageFunc')}>
+          <Ionicons name="people" size={24} color="rgba(0,0,0,.5)" />
+          <Text>Funcionarios</Text>
+        </TouchableOpacity>
+      </View> */}
+
+    </Box>
+  )
 }
 
 const styled = StyleSheet.create({
-    textTitle:{
-      color: 'black',
-      fontSize: 22,
-      fontWeight: 'bold',
-    },
-    ViewMenu:{
-        flex:1,
-        padding: 20
-    },
-    buttons:{
-        padding: 10,
-        height: 100,
-        width: 110,
-        margin: 30,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'lightgray',
-        borderRadius: 20
-    }
-  })
+  textTitle: {
+    color: 'black',
+    fontSize: 22,
+    fontWeight: 'bold',
+  },
+  ViewMenu: {
+    flex: 1,
+    padding: 20
+  },
+  buttons: {
+    padding: 10,
+    height: 100,
+    width: 110,
+    margin: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'lightgray',
+    borderRadius: 20
+  }
+})
